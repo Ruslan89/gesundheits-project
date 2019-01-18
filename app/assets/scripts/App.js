f1b235438ecc536add1eb1344324a43508887268
Ruslan <sugamonkeee@gmail.com>
2019-01-18 18:57:05 +0100

    Reveal on Scroll

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,12 @@
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
 
-var mobileMenu = new MobileMenu(); 
+var mobileMenu = new MobileMenu();
+new RevealOnScroll($(".feature-item"), "85%");
+new RevealOnScroll($(".section-title"), "85%");
+new RevealOnScroll($(".testimonial"), "60%");
+new RevealOnScroll($(".headline"), "85%");
+new RevealOnScroll($('p'), "60%");
+new RevealOnScroll($('img'), "60%");
+
